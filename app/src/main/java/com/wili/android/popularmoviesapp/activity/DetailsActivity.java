70fefd4eb0045357e59c5eaa45b32287ad4136b7
damwilin <damian.wilinski@gmail.com>
2018-02-24 18:48:37 +0100

    Layout created

--- app/src/main/java/com/wili/android/popularmoviesapp/activity/DetailsActivity.java
+++ app/src/main/java/com/wili/android/popularmoviesapp/activity/DetailsActivity.java
@@ -1,15 +1,32 @@
 package com.wili.android.popularmoviesapp.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.wili.android.popularmoviesapp.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class DetailsActivity extends AppCompatActivity {
+
+    @BindView(R.id.movie_title)
+    TextView title;
+    @BindView(R.id.movie_poster)
+    ImageView poster;
+    @BindView(R.id.movie_release_date)
+    TextView releaseDate;
+    @BindView(R.id.movie_vote_average)
+    TextView voteAverage;
+    @BindView(R.id.movie_plot_synopsis)
+    TextView plotSynopsis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        ButterKnife.bind(this);
     }
 }
--- app/src/main/java/com/wili/android/popularmoviesapp/activity/MainActivity.java
+++ app/src/main/java/com/wili/android/popularmoviesapp/activity/MainActivity.java
@@ -2,8 +2,13 @@ package com.wili.android.popularmoviesapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.wili.android.popularmoviesapp.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
