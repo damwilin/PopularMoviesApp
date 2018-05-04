d9632074bd0acdf37f695fe1fe4b08e9cad45617
damwilin <damian.wilinski@gmail.com>
2018-05-04 20:13:04 +0200

    Created MoviesProvider

--- app/src/main/java/com/wili/android/popularmoviesapp/repository/database/MoviesDbHelper.java
+++ app/src/main/java/com/wili/android/popularmoviesapp/repository/database/MoviesDbHelper.java
@@ -15,7 +15,7 @@ public class MoviesDbHelper extends SQLiteOpenHelper {
     static final String DATABASE_NAME = "movies.db";
     static final int DATABASE_VERSION = 1;
 
-    public MoviesDbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+    public MoviesDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
